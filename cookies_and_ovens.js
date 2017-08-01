24ec85c4a640bae34bff242f6847c9da3568f580
alfandash <cumiasem91@gmail.com>
2017-08-01 14:05:39 +0700

    release 1 done

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,65 @@
 //
 //
 // Your code here
+
+
+class Kue{
+  constructor(nama,rasa){
+    this._status = `mentah`
+    this._name = nama
+    this._rasa = rasa
+    this._lamaMasak = 0
+  }
+
+  panggang(){
+    this._status = ``
+  }
+}
+
+class KueCokelat extends Kue {
+  constructor(nama,rasa){
+    super(nama,rasa)
+    this._lamaMasak = 20
+  }
+}
+
+
+class KueKacang extends Kue {
+  constructor(nama,rasa){
+    super(nama,rasa)
+    this._lamaMasak = 30
+  }
+}
+
+class KueKeju extends Kue {
+  constructor(nama,rasa) {
+    super(nama,rasa)
+    this._lamaMasak = 35
+  }
+}
+
+class PabrikKue {
+  constructor(){}
+
+  static produksi(daftarKueArr){
+    var result = []
+    for (var i = 0; i < daftarKueArr.length; i++) {
+      if (daftarKueArr[i] === 'kue cokelat') {
+        result.push(new KueCokelat(`kue cokelat`,`cokelat`))
+      } else if (daftarKueArr[i] === 'kue kacang') {
+        result.push(new KueKacang(`kue kacang`,`kacang`))
+      } if (daftarKueArr[i] === `kue keju`) {
+        result.push(new KueKeju(`kue keju`,`keju`))
+      }
+    }
+
+    return result
+
+  }
+}
+
+var daftar1 = [`kue kacang`,`kue keju`,`kue cokelat`]
+var pesanan1 = PabrikKue.produksi(daftar1)
+
+
+console.log(pesanan1);

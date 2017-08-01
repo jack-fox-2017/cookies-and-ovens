9bd1f2874c2c364edd87144a0bc4c3ce8c6b6776
alfandash <cumiasem91@gmail.com>
2017-08-01 15:30:41 +0700

    release 1 and 2 done

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -12,61 +12,83 @@
 
 class Kue{
   constructor(nama,rasa){
-    this._status = `mentah`
+    this._status = 0
     this._name = nama
     this._rasa = rasa
-    this._lamaMasak = 0
-  }
-
-  panggang(){
-    this._status = ``
   }
 }
 
 class KueCokelat extends Kue {
-  constructor(nama,rasa){
-    super(nama,rasa)
+  constructor(){
+    super(`kue cokelat`,`cokelat`)
     this._lamaMasak = 20
   }
 }
 
 
 class KueKacang extends Kue {
-  constructor(nama,rasa){
-    super(nama,rasa)
+  constructor(){
+    super(`kue kacang`,`kacang`)
     this._lamaMasak = 30
   }
 }
 
 class KueKeju extends Kue {
-  constructor(nama,rasa) {
-    super(nama,rasa)
+  constructor() {
+    super(`kue keju`,`keju`)
     this._lamaMasak = 35
   }
 }
 
 class PabrikKue {
   constructor(){}
-
   static produksi(daftarKueArr){
     var result = []
     for (var i = 0; i < daftarKueArr.length; i++) {
       if (daftarKueArr[i] === 'kue cokelat') {
-        result.push(new KueCokelat(`kue cokelat`,`cokelat`))
+        result.push(new KueCokelat())
       } else if (daftarKueArr[i] === 'kue kacang') {
-        result.push(new KueKacang(`kue kacang`,`kacang`))
+        result.push(new KueKacang())
       } if (daftarKueArr[i] === `kue keju`) {
-        result.push(new KueKeju(`kue keju`,`keju`))
+        result.push(new KueKeju())
       }
     }
-
     return result
+  }
+}
 
+
+class Panggangan {
+  constructor(pesanan){
+    this._pesanan=pesanan
+  }
+  panggang(waktuPanggang){
+    var status = [`mentah`,`hampir matang`,`matang`,'gosong']
+
+    for (var i = 0; i <= waktuPanggang; i+=5) {
+      console.log(`Panggang menit ke ${i}`);
+
+      for (var j = 0; j < this._pesanan.length; j++) {
+
+        let step = this._pesanan[j][`_lamaMasak`] / 2
+
+        if (i > this._pesanan[j][`_lamaMasak`]) {
+          this._pesanan[j][`_status`] = 3
+        } else if ( i/step >= this._pesanan[j][`_status`]+1) {
+          this._pesanan[j][`_status`] ++
+        }
+        var tempStatus = status[this._pesanan[j][`_status`]]
+        console.log(`${this._pesanan[j][`_name`]} --> ${tempStatus}`);
+      }
+      console.log(``);
+    }
   }
 }
 
 var daftar1 = [`kue kacang`,`kue keju`,`kue cokelat`]
 var pesanan1 = PabrikKue.produksi(daftar1)
-
-
 console.log(pesanan1);
+console.log(`-------------`);
+
+var kueMasukPanggangan = new Panggangan(pesanan1)
+kueMasukPanggangan.panggang(35);
